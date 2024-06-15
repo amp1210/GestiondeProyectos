c3b462e415ce6d17e8ff6a7c890372c2e35d9643
Alexander <alfonso.martinez.ext@gmail.com>
2024-06-15 11:27:13 -0500

    sse agrega archivo de logica para agregar subregiones

---
+++ logica/subregion.py
@@ -0,0 +1,15 @@
+
+class Subregion:
+
+    def __init__(self, codigo, nombre):
+        self.codigo = codigo
+        self.nombre = nombre
+    
+    def editar():
+        pass
+
+    def guardar():
+        pass
+
+    def ver():
+        pass
